3aa80e75787dd63f5bbbb2cbd3ce27be7db0bd4b
ATAGWE ROGER <atagweroger2@gmail.com>
2026-02-18 11:25:48 +0100

    setup my backend and did some testing

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
+app.use(express.json());
+
+
+app.get("/test", (req, res) => {
+  res.json({ message: "API is running 🚀" });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
